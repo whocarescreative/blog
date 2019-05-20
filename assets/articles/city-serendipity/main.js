2c809f7c4348ce4510fa5c65acb9df28c9a2b8de
Max Kurapov <maxkurapov@gmail.com>
2019-05-20 13:54:36 +0200

    remove unnecessary files

--- assets/articles/city-serendipity/main.js
+++ assets/articles/city-serendipity/main.js
@@ -1,6 +1,4 @@
 import {} from './globe.js';
-import {} from './annotated_schedule.js';
 import {} from './eiffel_lean.js';
 import {} from './dice.js';
-import {} from './suggestion.js';
 import {} from './sounds.js';
